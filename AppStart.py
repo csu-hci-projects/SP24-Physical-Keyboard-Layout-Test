77c3a6bd0bef6bf42d218b8d32d30c6079f8c403
StumpyLabs <stumpf80369@gmail.com>
2024-03-26 02:17:44 -0600

    keyboard layout transfer from local machine

--- AppStart.py
+++ AppStart.py
@@ -10,31 +10,39 @@ def startApp(root):
     root.geometry("1000x500")
     root.resizable(width=False, height=False)
 
+    # Padding from top
     label = Label(root, text="")
     label.pack(padx=(0, 50), pady=(50, 0))
 
+    # Welcome Label
     header = Label(root, text="Welcome to our keyboard layout test", font=("Cambria", 20, "bold"))
     header.place(y=10)
     header.pack()
 
+    # Padding
     label = Label(root, text="")
     label.pack(padx=(0, 10), pady=(10, 0))
 
+    # Please Select label
     selection = Label(root, text="Please select a keyboard layout to get started", font=("Cambria", 20, "bold"))
     selection.place(y=30)
     selection.pack()
 
+    # Padding
     label = Label(root, text="")
     label.pack(padx=(0, 10), pady=(10, 0))
 
+    #Select Button
     selectedLayout = StringVar(root)
     selectedLayout.set("--Select a layout--")
     layoutMenu = OptionMenu(root, selectedLayout, "--Select a layout--", "QWERTY", "Dvorak", "Colemak", "Workman")
     layoutMenu.pack()
 
+    # Padding
     label = Label(root, text="")
     label.pack(padx=(0, 5), pady=(5, 0))
 
+    #Start Button
     def startApp():
         layout = selectedLayout.get()
         if layout == "--Select a layout--":
